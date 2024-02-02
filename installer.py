32eefd32dbd9b7838091dd16d2efcbb3ac510c20
Nathan Price <gravityfargo@gmail.com>
2024-02-01 21:11:53 -0500

    changed

--- installer.py
+++ installer.py
@@ -4,7 +4,10 @@ from git import Repo
 
 def main():
     vault_dir = os.path.dirname(os.path.realpath(__file__))
-    obsidian_dir = f"{vault_dir}/.obsidian"
+    vault_dir_split = vault_dir.split("/")
+    vault_dir = vault_dir.replace(vault_dir_split[-1], "")
+    
+    obsidian_dir = f"{vault_dir}.obsidian"
     community_plugins_config = f"{obsidian_dir}/community-plugins.json"
     required_plugins = [
         "templater-obsidian",
@@ -17,16 +20,9 @@ def main():
     required_dirs = [
             f"{obsidian_dir}/scripts/python/"
     ]
-    repo_url = "https://github.com/gravityfargo/Python-Obsidian-Manager.git"
-
-
     checkForPlugins(community_plugins_config, required_plugins)
-    clone_repo(repo_url, )
-
-
 
 def checkForPlugins(community_plugins_config, required_plugins):
-
     if os.path.isfile(community_plugins_config):
         with open(community_plugins_config, "r") as file:
             content = file.read()
@@ -37,14 +33,6 @@ def checkForPlugins(community_plugins_config, required_plugins):
             print("Required plugins are not installed and or enabled.")
     else:
         print("Required plugins are not installed and or enabled.")
-
-def clone_repo(repo_url, destination):
-    try:
-        Repo.clone_from(repo_url, destination)
-        print(f"Repository cloned into: {destination}")
-    except Exception as e:
-        print(f"Failed to clone repository: {e}")
-
 
 
 if __name__ == "__main__":
